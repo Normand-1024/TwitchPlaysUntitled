751fc86e0e580cd86211dec69061e35617cc7e37
Charles George <charles@jumprampgames.com>
2019-01-27 16:08:09 -0500

    walk sounds

--- Phaser/src/states/Game.js
+++ Phaser/src/states/Game.js
@@ -130,9 +130,7 @@ export default class extends Phaser.State {
     this.game.add.existing(this.smartflyGroup)
     this.game.add.existing(this.averagedPlayerController)
 
-    this.game.physics.arcade.enable([this.averagedPlayerController, this.waterGroup, this.flyGroup, this.smartflyGroup]);
-
-    this.game.physics.arcade.enable([this.averagedPlayerController, this.waterGroup, this.flyGroup, this.home]);
+    this.game.physics.arcade.enable([this.averagedPlayerController, this.waterGroup, this.flyGroup, this.smartflyGroup,this.home]);
     this.home.body.immovable = true;
     this.testWebSocket();
 
@@ -140,6 +138,9 @@ export default class extends Phaser.State {
     this.bmpText = game.add.bitmapText(10, 10, 'gem', flyCount + " / 10 Flies", 30);
 
     this.setupGameTimer();
+
+    this.startMusic()
+    this.attachSounds();
   }
 
   update() {
@@ -214,6 +215,14 @@ export default class extends Phaser.State {
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+
+
+  startMusic(){
+    this.music = game.add.audio('music');
+
+    this.music.play();
+  }
+
 
   placeMapTiles(){
     this.waterGroup = game.add.physicsGroup();
@@ -316,6 +325,7 @@ export default class extends Phaser.State {
   playerFlyCollision(player, fly){
     //fly.center_x = -1000000;
     fly.destroy();
+    player.game.slurpSound.play();
     flyCount++;
     //this.averagedPlayerController.x = 500;
     //this.averagedPlayerController.y = 500;
@@ -358,4 +368,7 @@ export default class extends Phaser.State {
     this.gameTimer.start();
   }
 
+  attachSounds(){
+    this.game.slurpSound = this.game.add.audio("slurp");
+  }
 }
