96a887fef9574f5708c067f32f4db89e59052c61
Preston <slethik@gmail.com>
2019-01-27 13:45:27 -0500

    Run average

--- aggregator/app.py
+++ aggregator/app.py
@@ -1,6 +1,8 @@
 import os
+import time
 import logging
 import redis
+import math
 import gevent
 from flask import Flask, render_template
 from flask_sockets import Sockets
@@ -8,6 +10,9 @@ from flask_sockets import Sockets
 REDIS_URL = os.environ['REDIS_URL']
 REDIS_CHAN = 'chat'
 REDIS_COMMAND_CHAN = 'command'
+
+CHUNKING_DIVISOR = 1
+QUEUE_TIMESTEPS = .1
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
@@ -27,7 +32,6 @@ class GameBackend(object):
         for message in self.pubsub.listen():
             data = message.get('data')
             if message['type'] == 'message':
-                app.logger.info(u'Sending message: {}'.format(data))
                 yield data
 
     def register(self, client):
@@ -44,14 +48,50 @@ class GameBackend(object):
             self.clients.remove(client)
 
     def run(self):
-        """Listens for new messages in Redis, and sends them to clients."""
         for data in self.__iter_data():
             for client in self.clients:
                 gevent.spawn(self.send, client, data)
 
+    def run_average(self):
+        """Listens for new messages in Redis, and sends them to clients."""
+        queue = []
+        import json
+        while True:
+            message = self.pubsub.get_message()
+            while message:
+                if message['type'] == 'message':
+                    queue.append(json.loads(message['data']))
+                message = self.pubsub.get_message()
+            length_of_queue = len(queue)
+            num_to_process = math.ceil(length_of_queue/CHUNKING_DIVISOR)
+
+            if num_to_process != 0:
+                directions = {
+                    'up': 0,
+                    'down': 0,
+                    'left': 0,
+                    'right': 0
+                }
+                for message in queue[0:num_to_process]:
+                    if not message.get('direction'):
+                        continue
+                    if message['direction'] == 'up':
+                        directions['up'] += 1
+                    elif message['direction'] == 'down':
+                        directions['down'] += 1
+                    elif message['direction'] == 'left':
+                        directions['left'] += 1
+                    elif message['direction'] == 'right':
+                        directions['right'] += 1
+
+                for client in self.clients:
+                    gevent.spawn(self.send, client, directions)
+                queue = queue[num_to_process+1:]
+            time.sleep(QUEUE_TIMESTEPS)
+
     def start(self):
         """Maintains Redis subscription in the background."""
-        gevent.spawn(self.run)
+        gevent.spawn(self.run_average)
 
 game_backend = GameBackend()
 game_backend.start()
